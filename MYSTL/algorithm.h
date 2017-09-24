a745c2288a0117006e2cd40b401bfc71e9d839f8
CK <1051818459@qq.com>
2017-09-24 13:55:16 +0800

    增加算法

--- MYSTL/algorithm.h
+++ MYSTL/algorithm.h
@@ -3,10 +3,16 @@
 
 namespace mySTL {
 	template <class T>
-	T max(T& a, T& b) {
+	T max(const T& a, const T& b) {
 		return (a > b) ? a : b;
 	}
 
+	template <class T>
+	void swap(T& a, T& b) {
+		T t = a;
+		a = b;
+		b = t;
+	}
 
 	template<class OutputIterator, class Size, class T>
 	OutputIterator fill_n(OutputIterator first, Size n, const T& value)
