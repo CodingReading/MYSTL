bfe148ae65f0f681a15dad0c79bdcd67f057be41
CK <1051818459@qq.com>
2017-09-27 17:09:15 +0800

    增加list测试

--- MYSTL/main.cpp
+++ MYSTL/main.cpp
@@ -1,9 +1,10 @@
 #include "UnitTest\typetraits_test.h"
 #include "UnitTest\vector_test.h"
+#include "UnitTest\list_test.h"
 
 int main() {
 	mySTL::typetraits_test::test_all();
 	mySTL::vector_test::test_all();
-
+	mySTL::list_test::test_all();
 	return 0;
 }
