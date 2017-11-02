8ebaf6aee509ba2b77a0032df3d3d1a2eb4661ea
CK <1051818459@qq.com>
2017-11-02 20:25:06 +0800

    主函数

--- MYSTL/main.cpp
+++ MYSTL/main.cpp
@@ -1,11 +1,13 @@
 //#include "UnitTest\typetraits_test.h"
 //#include "UnitTest\vector_test.h"
 //#include "UnitTest\list_test.h"
-#include "UnitTest\deque_test.h"
+//#include "UnitTest\deque_test.h"
+#include "UnitTest\stack_test.h"
 int main() {
     //mySTL::typetraits_test::test_all();
     //mySTL::vector_test::test_all();
-   // mySTL::list_test::test_all();
-    mySTL::deque_test::test_all();
+    //mySTL::list_test::test_all();
+    //mySTL::deque_test::test_all();
+    mySTL::stack_test::test_all();
     return 0;
 }
