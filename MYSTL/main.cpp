330c0ef57efd97acb93188c3187c7784aad4ded2
CK <1051818459@qq.com>
2017-11-03 18:26:44 +0800

    加入queue测试

--- MYSTL/main.cpp
+++ MYSTL/main.cpp
@@ -2,12 +2,14 @@
 //#include "UnitTest\vector_test.h"
 //#include "UnitTest\list_test.h"
 //#include "UnitTest\deque_test.h"
-#include "UnitTest\stack_test.h"
+//#include "UnitTest\stack_test.h"
+#include "UnitTest\queue_test.h"
 int main() {
     //mySTL::typetraits_test::test_all();
     //mySTL::vector_test::test_all();
     //mySTL::list_test::test_all();
     //mySTL::deque_test::test_all();
-    mySTL::stack_test::test_all();
+    //mySTL::stack_test::test_all();
+    mySTL::queue_test::test_all();
     return 0;
 }
