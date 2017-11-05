e50527cdc9f1dc92b46666b9c18cef916aab3d93
CK <1051818459@qq.com>
2017-11-05 16:02:51 +0800

    加入heap test

--- MYSTL/main.cpp
+++ MYSTL/main.cpp
@@ -3,13 +3,15 @@
 //#include "UnitTest\list_test.h"
 //#include "UnitTest\deque_test.h"
 //#include "UnitTest\stack_test.h"
-#include "UnitTest\queue_test.h"
+//#include "UnitTest\queue_test.h"
+#include "UnitTest\heap_test.h"
 int main() {
     //mySTL::typetraits_test::test_all();
     //mySTL::vector_test::test_all();
     //mySTL::list_test::test_all();
     //mySTL::deque_test::test_all();
     //mySTL::stack_test::test_all();
-    mySTL::queue_test::test_all();
+    //mySTL::queue_test::test_all();
+    mySTL::heap_test::test_all();
     return 0;
 }
